7aa2a626c6e92668fa0f351cce75acb0336adabd
kiran <kiran@kiran-Aspire-A715-42G>
2022-03-21 00:11:48 +0530

    Done

---
+++ CarObjectProgram/src/CarObj.java
@@ -0,0 +1,28 @@
+
+class car1
+{
+	void Display()
+	{
+		System.out.println("BMW");
+		
+	}
+}
+
+class car2
+{
+	void Display()
+	{
+		System.out.println("Audi");
+	}
+}
+
+public class CarObj
+{
+	public static void main(String[] args) 
+	 {
+		
+		car1 c = new car1();
+		c.Display();
+	 }
+
+}
